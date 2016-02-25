2717eb8f065b87bbcc7a93de926289bb8d9c823b
Markus Reiter-Haas <iseratho@tug-swl-227-46.tu-graz.ac.at>
2016-02-25 15:27:51 +0100

    Refactoring done

--- src/main/java/org/catrobat/estimationplugin/calc/EstimationCalculator.java
+++ src/main/java/org/catrobat/estimationplugin/calc/EstimationCalculator.java
@@ -3,19 +3,14 @@ package org.catrobat.estimationplugin.calc;
 import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.issue.CustomFieldManager;
 import com.atlassian.jira.issue.Issue;
-import com.atlassian.jira.issue.IssueManager;
 import com.atlassian.jira.issue.changehistory.ChangeHistory;
 import com.atlassian.jira.issue.changehistory.ChangeHistoryManager;
 import com.atlassian.jira.issue.customfields.option.Option;
 import com.atlassian.jira.issue.fields.CustomField;
 import com.atlassian.jira.issue.search.SearchException;
 import com.atlassian.jira.issue.search.SearchProvider;
-import com.atlassian.jira.issue.search.SearchResults;
-import com.atlassian.jira.project.Project;
 import com.atlassian.jira.project.ProjectManager;
 import com.atlassian.jira.user.ApplicationUser;
-import com.atlassian.jira.web.bean.PagerFilter;
-import com.atlassian.query.Query;
 import org.catrobat.estimationplugin.jql.IssueListCreator;
 import org.ofbiz.core.entity.GenericValue;
 
@@ -26,8 +21,6 @@ import java.util.*;
 public class EstimationCalculator {
 
     private final ProjectManager projectManager;
-    private final SearchProvider searchProvider;
-    private final ApplicationUser user;
 
     private float ticketsPerDay;
     private float averageTicketDurationDays;
@@ -44,8 +37,6 @@ public class EstimationCalculator {
 
     public EstimationCalculator(ProjectManager projectManager, SearchProvider searchProvider, ApplicationUser user) {
         this.projectManager = projectManager;
-        this.searchProvider = searchProvider;
-        this.user = user;
         issueListCreator = new IssueListCreator(searchProvider, user);
 
         loadSettings();
@@ -63,10 +54,10 @@ public class EstimationCalculator {
         defaultEstimate = 5;
     }
 
-    public void calculateTicketsPerDay(Long projectId) throws SearchException
+    public void calculateTicketsPerDay()
     {
-        ticketsPerDay = getFinishedIssueCount(projectId)/((float)getProjectDurationFromStart(projectId));
-        averageTicketDurationDays = getDaysTicketsWhereOpened(projectId)/((float)getFinishedIssueCount(projectId));
+        ticketsPerDay = getFinishedIssueCount()/((float)getProjectDurationFromStart());
+        averageTicketDurationDays = getDaysTicketsWhereOpened()/((float)getFinishedIssueCount());
     }
 
     public int uncertainty()
@@ -74,17 +65,15 @@ public class EstimationCalculator {
         return 7;
     }
 
-    public long getProjectDurationFromStart(Long projectId) throws SearchException{
-        long start = getProjectStartInMillis(projectId);
+    public long getProjectDurationFromStart() {
+        long start = getProjectStartInMillis();
         Date today = new Date();
         long days = (today.getTime() - start)/(1000 * 60 * 60 * 24);
         return  days;
     }
 
-    public long getProjectStartInMillis(Long projectId) throws SearchException {
-        Query query = issueListCreator.getQueryWithIssueStatus(projectId, finishedIssuesStatus);
-        SearchResults searchResults = searchProvider.search(query, user, PagerFilter.getUnlimitedFilter());
-        List<Issue> issues = searchResults.getIssues();
+    public long getProjectStartInMillis() {
+        List<Issue> issues = finishedIssueList;
         ListIterator<Issue> issueIterator = issues.listIterator();
         Timestamp minCreated = issueIterator.next().getCreated();
         while (issueIterator.hasNext()) {
@@ -97,8 +86,8 @@ public class EstimationCalculator {
         return minCreated.getTime();
     }
 
-    public Date getProjectStartDate(Long projectId) throws SearchException{
-        return new Date(getProjectStartInMillis(projectId));
+    public Date getProjectStartDate() {
+        return new Date(getProjectStartInMillis());
     }
 
 
@@ -126,15 +115,12 @@ public class EstimationCalculator {
         openIssueList = issueListCreator.getIssueListForStatus(projectId, openIssuesStatus);
         finishedIssueList = issueListCreator.getIssueListForStatus(projectId, finishedIssuesStatus);
 
-        calculateTicketsPerDay(projectId);
+        calculateTicketsPerDay();
         int uncertainty = uncertainty();
 
         Date today = new Date();
-        Calendar epochStart = Calendar.getInstance();
-        epochStart.set(1970, 01, 01);
-        Date startDate = epochStart.getTime();
-        long openIssues = getOpenIssueCount(projectId);
-        long openCost = getOpenIssueCost(projectId);
+        long openIssues = getOpenIssueCount();
+        long openCost = getOpenIssueCost();
         float daysToFinish = openIssues / ticketsPerDay;
         int daysToFinishRounded = Math.round(daysToFinish);
         Calendar finishDate = Calendar.getInstance();
@@ -146,52 +132,21 @@ public class EstimationCalculator {
         data.put("openCost", openCost);
         data.put("finishDate", finishDate.getTime());
         data.put("uncertainty", uncertainty);
-        String ticketsPerDay = String.valueOf(getFinishedIssueCount(projectId)) +
-                "/" +  String.valueOf(getDaysTicketsWhereOpened(projectId) + "/" + String.valueOf(getProjectDurationFromStart(projectId)));
+        String ticketsPerDay = String.valueOf(getFinishedIssueCount()) +
+                "/" +  String.valueOf(getDaysTicketsWhereOpened() + "/" + String.valueOf(getProjectDurationFromStart()));
         data.put("ticketsPerDay", ticketsPerDay);
-        data.put("costMap", getOpenIssueCostMap(projectId));
-        data.put("smlMap", getOpenIssueSMLMap(projectId));
-        IssueManager issueManager = ComponentAccessor.getIssueManager();
-        data.put("test", getChangeItemExample(issueManager.getIssueByCurrentKey("WEB-244")));
-        data.put("test2", getStringExample(issueManager.getIssueByCurrentKey("WEB-244")));
+        data.put("costMap", getMapOfEffortsFromIssueListForCustomField(openIssueList, estimationField));
+        data.put("smlMap", getMapOfEffortsFromIssueListForCustomField(openIssueList, estimationSMLField));
         data.put("avgDaysOpened", averageTicketDurationDays);
         finishDate.setTime(today);
         finishDate.add(Calendar.DATE,Math.round(averageTicketDurationDays));
         data.put("avgFinishDate", finishDate.getTime());
-        data.put("projectStart", getProjectStartDate(projectId));
+        data.put("projectStart", getProjectStartDate());
 
         return data;
     }
 
-    private List<GenericValue> getChangeItemExample(Issue issue) {
-        ChangeHistoryManager changeHistoryManager = ComponentAccessor.getChangeHistoryManager();
-        List<ChangeHistory> issueHistory = changeHistoryManager.getChangeHistories(issue);
-        return issueHistory.get(0).getChangeItems();
-    }
-
-    private String getStringExample(Issue issue) {
-        ChangeHistoryManager changeHistoryManager = ComponentAccessor.getChangeHistoryManager();
-        List<ChangeHistory> issueHistory = changeHistoryManager.getChangeHistories(issue);
-        List<GenericValue> changeItems = issueHistory.get(0).getChangeItems();
-        String teststring = "";
-        int count = 0;
-        for(GenericValue genericValue : changeItems) {
-            String field = genericValue.getString("field");
-            teststring += field + " ";
-            String oldstring = genericValue.getString("oldstring");
-            teststring += oldstring + " ";
-            String newstring = genericValue.getString("newstring");
-            teststring += newstring + " ";
-            count++;
-            if (field.equals("status") && oldstring.equals("Issues Pool") && newstring.equals("Backlog")) {
-                teststring += "ok";
-            }
-        }
-        teststring += issueHistory.get(0).getTimePerformed().toString();
-        return teststring + " " + count;
-    }
-
-    private Timestamp getDatePutIntoBacklog(Issue issue) throws SearchException{
+    private Timestamp getDatePutIntoBacklog(Issue issue) {
         ChangeHistoryManager changeHistoryManager = ComponentAccessor.getChangeHistoryManager();
         List<ChangeHistory> issueHistory = changeHistoryManager.getChangeHistories(issue);
         for(ChangeHistory changeHistory : issueHistory) {
@@ -211,10 +166,8 @@ public class EstimationCalculator {
     }
 
     // TODO: change to be based on date put into backlog
-    private long getDaysTicketsWhereOpened(Long projectId) throws SearchException{
-        Query query = issueListCreator.getQueryWithIssueStatus(projectId, finishedIssuesStatus);
-        SearchResults searchResults = searchProvider.search(query, user, PagerFilter.getUnlimitedFilter());
-        List<Issue> issues = searchResults.getIssues();
+    private long getDaysTicketsWhereOpened() {
+        List<Issue> issues = finishedIssueList;
         ListIterator<Issue> issueIterator = issues.listIterator();
         long daysOpened = 0;
         while (issueIterator.hasNext()) {
@@ -232,8 +185,7 @@ public class EstimationCalculator {
     }
 
     private Map<String, Long> getMapOfEffortsFromIssueListForCustomField(List<Issue> issueList, CustomField customField) {
-        List<Issue> issues = issueList;
-        ListIterator<Issue> issueIterator = issues.listIterator();
+        ListIterator<Issue> issueIterator = issueList.listIterator();
         Map<String, Long> map = new HashMap<String, Long>();
         while (issueIterator.hasNext()) {
             Issue currentIssue = issueIterator.next();
@@ -246,35 +198,8 @@ public class EstimationCalculator {
         return map;
     }
 
-    private Map<String, Long> getMapForQueryForCustomField(Query query, CustomField customField) throws SearchException {
-        SearchResults searchResults = searchProvider.search(query, user, PagerFilter.getUnlimitedFilter());
-        List<Issue> issues = searchResults.getIssues();
-        ListIterator<Issue> issueIterator = issues.listIterator();
-        Map<String, Long> map = new HashMap<String, Long>();
-        while (issueIterator.hasNext()) {
-            Issue currentIssue = issueIterator.next();
-            if (currentIssue.getCustomFieldValue(customField) != null && currentIssue.getCustomFieldValue(customField) instanceof Option) {
-                String value = ((Option) currentIssue.getCustomFieldValue(customField)).getValue();
-                map.putIfAbsent(value, new Long(0));
-                map.put(value, map.get(value) + 1);
-            }
-        }
-        return map;
-    }
-
-    private Map<String, Long> getOpenIssueCostMap(Long projectId) throws SearchException {
-        Query query = issueListCreator.getQueryWithIssueStatus(projectId, openIssuesStatus);
-        return getMapForQueryForCustomField(query, estimationField);
-    }
-
-    private Map<String, Long> getOpenIssueSMLMap(Long projectId) throws SearchException {
-        Query query = issueListCreator.getQueryWithIssueStatus(projectId, openIssuesStatus);
-        return getMapForQueryForCustomField(query, estimationSMLField);
-    }
-
-    // TODO: based on list
-    private long getOpenIssueCost(Long projectId) throws SearchException {
-        Map<String, Long> map = getOpenIssueCostMap(projectId);
+    private long getOpenIssueCost() {
+        Map<String, Long> map = getMapOfEffortsFromIssueListForCustomField(openIssueList, estimationField);
         long sumEstimates = 0;
         for (Map.Entry<String, Long> entry : map.entrySet()) {
             long key;
@@ -288,11 +213,11 @@ public class EstimationCalculator {
         return sumEstimates;
     }
 
-    private long getOpenIssueCount(Long projectId) throws SearchException {
+    private long getOpenIssueCount() {
         return openIssueList.size();
     }
 
-    private long getFinishedIssueCount(Long projectId) throws SearchException{
+    private long getFinishedIssueCount() {
         return finishedIssueList.size();
     }
 }
